a74ec8f8703a82e11994a9dd9228121de3225d64
Hezekiah Calub <hcalub87@gmail.com>
2026-06-29 23:31:55 +0100

    Navbar Styling Complete, Missing Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 function Navbar() {
     return (
-        <nav className="bg-gray-800 p-4">
+        <nav className="bg-white p-4">
             <div className="container mx-auto flex items-center justify-between">
-                <div className="text-white font-bold text-xl">Dorilicious</div>
-                <div className="space-x-4">
-                    <a href="/" className="text-gray-300 hover:text-white">Home</a>
-                    <a href="/about" className="text-gray-300 hover:text-white">About</a>
-                    <a href="/contact" className="text-gray-300 hover:text-white">Contact</a>
+                <div className="text-royal-purple font-inter text-xl">Logo</div>
+                <div className="flex items-center space-x-6">
+                    <a href="/" className="relative group text-royal-purple text-sm font-inter py-1">Home <span className="absolute bottom-0 left-1/2 w-0 h-[2px] bg-royal-purple transition-all duration-300 -translate-x-1/2 group-hover:w-full"></span></a>
+                    <a href="/gallery" className="relative group text-royal-purple text-sm font-inter py-1">Gallery <span className="absolute bottom-0 left-1/2 w-0 h-[2px] bg-royal-purple transition-all duration-300 -translate-x-1/2 group-hover:w-full"></span></a>
+                    <a href="/prices" className="relative group text-royal-purple text-sm font-inter py-1">Prices <span className="absolute bottom-0 left-1/2 w-0 h-[2px] bg-royal-purple transition-all duration-300 -translate-x-1/2 group-hover:w-full"></span></a>
+                    <a href="/contact" className="relative group text-royal-purple text-sm font-inter py-1">Contact <span className="absolute bottom-0 left-1/2 w-0 h-[2px] bg-royal-purple transition-all duration-300 -translate-x-1/2 group-hover:w-full"></span></a>
                 </div>
             </div>
         </nav>
